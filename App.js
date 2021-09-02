15db59612e1b15f449ada16ff71b591c5e98582e
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-02 15:25:50 -0700

    react query provider

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {NativeBaseProvider, StatusBar} from 'native-base';
 import ConfigRoutes from './src/screens/Config';
+import {QueryClient, QueryClientProvider} from 'react-query';
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <NativeBaseProvider>
-      <StatusBar backgroundColor="#fff" />
-      <ConfigRoutes />
+      <QueryClientProvider client={queryClient}>
+        <StatusBar backgroundColor="#fff" />
+        <ConfigRoutes />
+      </QueryClientProvider>
     </NativeBaseProvider>
   );
 };
