401544e7e15c9a58bef437eb0b641f4fb0573dca
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-02 15:23:58 -0700

    drawer navigation

--- src/screens/Config.js
+++ src/screens/Config.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 // import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 
 import * as screens from '../constant/routes';
@@ -9,18 +10,22 @@ import SplashScreen from './Splash';
 import SigninScreen from './auth/Signin';
 import RegisterScreen from './auth/Register';
 import HomeScreen from './Home';
-
-const Stack = createNativeStackNavigator();
+import CustomDrawer from '../components/common/Drawer';
+// const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 const MainStack = () => (
-  <Stack.Navigator
+  <Drawer.Navigator
+    drawerContent={CustomDrawer}
     screenOptions={{
       headerShown: false,
+      drawerPosition: 'right',
+      drawerStyle: {backgroundColor: 'transparent'},
     }}>
-    <Stack.Screen component={HomeScreen} name={screens.HOME_SCREEN} />
-    <Stack.Screen component={SplashScreen} name={screens.SPLASH_SCREEN} />
-    <Stack.Screen component={SigninScreen} name={screens.LOGIN_SCREEN} />
-    <Stack.Screen component={RegisterScreen} name={screens.REGISTER_SCREEN} />
-  </Stack.Navigator>
+    <Drawer.Screen component={HomeScreen} name={screens.HOME_SCREEN} />
+    <Drawer.Screen component={SplashScreen} name={screens.SPLASH_SCREEN} />
+    <Drawer.Screen component={SigninScreen} name={screens.LOGIN_SCREEN} />
+    <Drawer.Screen component={RegisterScreen} name={screens.REGISTER_SCREEN} />
+  </Drawer.Navigator>
 );
 
 const ConfigRoutes = () => (
