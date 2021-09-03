3fbd1af33a959f9e7f92cd72dcfe67301b9b7db2
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-03 12:30:00 -0700

    change email schema to phone

--- src/constant/schema.js
+++ src/constant/schema.js
@@ -1,6 +1,10 @@
 import * as Yup from 'yup';
 export const SIGNIN_VALIDATION = Yup.object({
-  email: Yup.string().required('required').email('invalid email'),
+  // email: Yup.string().required('required').email('invalid email'),
+  phone: Yup.string()
+    .required('required')
+    .matches(/^\d+$/, 'invalid phone')
+    .length(11, 'phone number is 11 characters'),
   password: Yup.string().required('required').min(6, 'at least 6 charachters'),
 });
 export const REGISTRATION_VALIDATION = Yup.object({
