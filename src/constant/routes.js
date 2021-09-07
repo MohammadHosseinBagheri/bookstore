befef40f57a29f626ea57a7e84b5fb17cd27bd58
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-07 15:44:18 -0700

    Detail screen

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -2,3 +2,4 @@ export const SPLASH_SCREEN = 'SPLASH_SCREEN';
 export const LOGIN_SCREEN = 'LOGIN_SCREEN';
 export const REGISTER_SCREEN = 'REGISTER_SCREEN';
 export const HOME_SCREEN = 'HOME_SCREEN';
+export const DETAIL_SCREEN = 'DETAIL_SCREEN';
