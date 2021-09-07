04c88fbd0aedfc3efbbb90a15ab8f174a6ed5b04
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-07 15:45:33 -0700

    navigate to detail screen

--- src/utils/home.js
+++ src/utils/home.js
@@ -1,3 +1,5 @@
-const handleRating=()=>{
-    
-}
+import * as screens from '../constant/routes';
+const handleRating = () => {};
+export const handleNavigateDetail = (navigation, data) => {
+  return navigation.push(screens.DETAIL_SCREEN, data);
+};
