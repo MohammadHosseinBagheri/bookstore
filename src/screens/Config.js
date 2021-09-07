6c6f666d5cddb535748564b187a976677d90c29c
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-07 15:44:49 -0700

    add react shared navigation

--- src/screens/Config.js
+++ src/screens/Config.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // import {createDrawerNavigator} from '@react-navigation/drawer';
-// import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
 
 import * as screens from '../constant/routes';
 import SplashScreen from './Splash';
@@ -13,7 +12,11 @@ import HomeScreen from './Home';
 import CustomDrawer from '../components/common/Drawer';
 import {useIsAuthState} from '../context/useIsLoggedIn';
 import {removeData} from '../helper/common';
-const Stack = createNativeStackNavigator();
+import {enableScreens} from 'react-native-screens';
+import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
+import BookDetailScreen from './BookDetail';
+enableScreens();
+const Stack = createSharedElementStackNavigator();
 // const Drawer = createDrawerNavigator();
 const MainStack = () => {
   const isLoggedIn = useIsAuthState();
@@ -22,14 +25,27 @@ const MainStack = () => {
     return (
       <Stack.Navigator
         initialRouteName={screens.HOME_SCREEN}
-        // drawerContent={CustomDrawer}
         screenOptions={{
           headerShown: false,
-          // drawerPosition: 'right',
-          // drawerStyle: {backgroundColor: 'transparent'},
-          // swipeEnabled: false,
         }}>
         <Stack.Screen component={HomeScreen} name={screens.HOME_SCREEN} />
+        <Stack.Screen
+          component={BookDetailScreen}
+          name={screens.DETAIL_SCREEN}
+          options={() => ({
+            headerShown: false,
+            gestureEnabled: false,
+            transitionSpec: {
+              open: {animation: 'timing', config: {duration: 1000}},
+              close: {animation: 'timing', config: {duration: 1000}},
+            },
+            cardStyleInterpolator: ({current: {progress}}) => ({
+              cardStyle: {
+                opacity: progress,
+              },
+            }),
+          })}
+        />
       </Stack.Navigator>
     );
   }
@@ -38,14 +54,11 @@ const MainStack = () => {
       initialRouteName={
         isLoggedIn ? screens.HOME_SCREEN : screens.SPLASH_SCREEN
       }
-      // drawerContent={CustomDrawer}
       screenOptions={{
         headerShown: false,
-        // drawerPosition: 'right',
-        // drawerStyle: {backgroundColor: 'transparent'},
-        // swipeEnabled: false,
       }}>
       <Stack.Screen component={HomeScreen} name={screens.HOME_SCREEN} />
+
       <Stack.Screen component={SplashScreen} name={screens.SPLASH_SCREEN} />
       <Stack.Screen component={SigninScreen} name={screens.LOGIN_SCREEN} />
       <Stack.Screen component={RegisterScreen} name={screens.REGISTER_SCREEN} />
